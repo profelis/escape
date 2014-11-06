6ee39d0e84ab39213e86d692a9240aa982e56e58
Dima Granetchi <system.grand@gmail.com>
2014-11-06 22:36:29 +0200

    add math bits

---
+++ src/deep/math/Bits.hx
@@ -0,0 +1,38 @@
+package deep.math;
+
+@:enum abstract Bits(Int) from Int to Int {
+    var B0 = 1<<0;
+    var B1 = 1<<1;
+    var B2 = 1<<2;
+    var B3 = 1<<3;
+    var B4 = 1<<4;
+    var B5 = 1<<5;
+    var B6 = 1<<6;
+    var B7 = 1<<7;
+    var B8 = 1<<8;
+    var B9 = 1<<9;
+    var B10 = 1<<10;
+    var B11 = 1<<11;
+    var B12 = 1<<12;
+    var B13 = 1<<13;
+    var B14 = 1<<14;
+    var B15 = 1<<15;
+    var B16 = 1<<16;
+    var B17 = 1<<17;
+    var B18 = 1<<18;
+    var B19 = 1<<19;
+    var B20 = 1<<20;
+    var B21 = 1<<21;
+    var B22 = 1<<22;
+    var B23 = 1<<23;
+    var B24 = 1<<24;
+    var B25 = 1<<25;
+    var B26 = 1<<26;
+    var B27 = 1<<27;
+    var B28 = 1<<28;
+    var B29 = 1<<29;
+    var B30 = 1<<30;
+    var B31 = 1<<31;
+
+    var ALL = -1;
+}
